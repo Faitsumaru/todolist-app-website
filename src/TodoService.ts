e4112c712d6f93614e3d4a20514de731123a533b
Faitsumaru <kiryakov.g.m@gmail.com>
2025-02-18 13:36:33 +0300

    creating CRUD funcs module

---
+++ src/TodoService.ts
@@ -0,0 +1,46 @@
+import TodoTypes from "./todo";
+
+
+const LOCAL_STORAGE_KEY = 'todos';
+
+const TodoService = {
+
+    //Get Todos
+    getTodos: (): TodoTypes[] => {
+        const todoStr = localStorage.getItem(LOCAL_STORAGE_KEY)
+
+        return todoStr ? JSON.parse(todoStr) : [];
+    },
+
+    //Adding Todos
+    addTodos: (text: string): TodoTypes => {
+        const todos = TodoService.getTodos();
+        const newTodo: TodoTypes = {
+            id: todos.length + 1,
+            text,
+            completed: false
+        };
+        const updateTodos = [...todos, newTodo];
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(updateTodos));
+
+        return newTodo;
+    },
+
+    //Updating the TODO
+    updateTodo: (todo: TodoTypes): TodoTypes => {
+        const todos = TodoService.getTodos();
+        const updateTodos = todos.map((t) => (t.id === todo.id ? todo : t))
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(updateTodos));
+
+        return todo;
+    },
+
+    //Deleting the TODO
+    deleteTodo: (id: number): void => {
+        const todos = TodoService.getTodos();
+        const updateTodos = todos.filter((todo) => todo.id !== id);
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(updateTodos));
+    }
+};
+
+export default TodoService;
